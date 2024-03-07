dcd058584a2a34d4d9173c1be522def94b78fe21
TajimaMiho <kapibaramihho@gmail.com>
2024-03-07 16:34:58 +0900

    feat: sound classified

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:tflite_audio/tflite_audio.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _sound = "Press the button to start";
+  bool _recording = false;
+  late Stream<Map<dynamic, dynamic>> result;
+
+  @override
+  void initState() {
+    super.initState();
+    TfliteAudio.loadModel(
+      model: 'assets/soundclassifier.tflite',
+      label: 'assets/labels.txt',
+      numThreads: 1,
+      isAsset: true,
+      inputType: 'rawAudio',
+    );
+  }
+
+  void _recorder() {
+    String recognition = "";
+    if (!_recording) {
+      setState(() => _recording = true);
+      result = TfliteAudio.startAudioRecognition(
+        numOfInferences: 1,
+        sampleRate: 44100,
+        bufferSize: 22016,
+      );
+      result.listen((event) {
+        recognition = event["recognitionResult"];
+      }).onDone(() {
+        setState(() {
+          _recording = false;
+          _sound = recognition.split(" ")[1];
+        });
+      });
+    }
+  }
+
+  void _stop() {
+    TfliteAudio.stopAudioRecognition();
+    setState(() => _recording = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "What's this sound?",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 60,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ),
+              MaterialButton(
+                onPressed: _recorder,
+                color: _recording ? Colors.grey : Colors.pink,
+                textColor: Colors.white,
+                child: Icon(Icons.mic, size: 60),
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(25),
+              ),
+              Text(
+                '$_sound',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
