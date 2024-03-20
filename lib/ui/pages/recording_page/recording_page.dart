48fbab822a15d1f5ba709062ffdbe2364cfb8822
TajimaMiho <kapibaramihho@gmail.com>
2024-03-20 23:26:09 +0900

    feat: sharedialog

--- lib/ui/pages/recording_page/recording_page.dart
+++ lib/ui/pages/recording_page/recording_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sound_classify_app/controllers/recording_page_controller.dart';
+import 'package:sound_classify_app/models/detail.dart';
 import 'package:sound_classify_app/thems/app_colors.dart';
 import 'package:sound_classify_app/ui/components/result_text.dart';
 import 'package:sound_classify_app/ui/components/small_radius_button.dart';
@@ -11,14 +12,20 @@ class RecordingPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    Detail? detail = ref.watch(recordingProvider).detail;
     return Scaffold(
       appBar: AppBar(
         title: const Text('SoundFit'),
         actions: [
           ref.watch(recordingProvider).isRecordingCompleted
               ? TextButton(
-                  onPressed:
-                      ref.read(recordingProvider.notifier).determinePosition,
+                  onPressed: () {
+                    ref.read(recordingProvider.notifier).determinePosition;
+                    showDialog(
+                      context: context,
+                      builder: (context) => ShareDetailDialog(detail: detail!),
+                    );
+                  },
                   child: const Icon(
                     Icons.ios_share,
                     size: 40,
@@ -34,11 +41,10 @@ class RecordingPage extends ConsumerWidget {
             buildRecordButton(ref),
             const ResultText(),
             ref.watch(recordingProvider).isRecordingCompleted
-                ? Text(ref.watch(recordingProvider).detail.toString())
-                /*SmallRadiusPinkButton(
+                ? SmallRadiusPinkButton(
                     text: const Text('ARで見る'),
                     onPressed: () => context.go('/home/ar'),
-                  )*/
+                  )
                 : const Text(
                     'ボタンを押してください',
                     style: TextStyle(fontSize: 32),
@@ -59,6 +65,31 @@ class RecordingPage extends ConsumerWidget {
       shape: const CircleBorder(),
       padding: const EdgeInsets.all(25),
       child: const Icon(Icons.mic, size: 60),
+    );
+  }
+}
+
+class ShareDetailDialog extends StatelessWidget {
+  const ShareDetailDialog({Key? key, required Detail this.detail})
+      : super(key: key);
+
+  final Detail detail;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('情報の共有'),
+      content: const Text('音声解析の結果と場所を共有しますか？'),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('キャンセル'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('共有'),
+        ),
+      ],
     );
   }
 }
