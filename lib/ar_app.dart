73392ebfa8bdf5bf6bdf117ff9628cff006ea6af
matsuo1234567 <matsuonanami773@gmail.com>
2024-03-22 19:46:00 +0900

    ar sample

---
+++ lib/ar_app.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
+
+// 音声認識のデータから音の大きさを取得する関数
+double getSoundVolume() {
+  // 音声認識のデータから音の大きさを取得する処理
+  return Random().nextDouble();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Christmas Decorations',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyArApp(),
+    );
+  }
+}
+
+class MyArApp extends StatefulWidget {
+  const MyArApp({Key? key}) : super(key: key);
+  @override
+  State<MyArApp> createState() => _MyArAppState();
+}
+
+class _MyArAppState extends State<MyArApp> {
+  late ARKitController arkitController;
+
+  @override
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Christmas Decorations')),
+        body: ARKitSceneView(
+          showFeaturePoints: true,
+          enableTapRecognizer: true,
+          planeDetection: ARPlaneDetection.horizontalAndVertical,
+          onARKitViewCreated: onARKitViewCreated,
+        ),
+      );
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    this.arkitController.onARTap = (ar) {
+      final point = ar.firstWhereOrNull(
+        (o) => o.type == ARKitHitTestResultType.featurePoint,
+      );
+      if (point != null) {
+        _onARTapHandler(point);
+      }
+    };
+  }
+
+  void _onARTapHandler(ARKitTestResult point) {
+    final position = vector.Vector3(
+      point.worldTransform.getColumn(3).x,
+      point.worldTransform.getColumn(3).y,
+      point.worldTransform.getColumn(3).z,
+    );
+
+    // 音声認識のデータから音の大きさを取得
+    double volume = getSoundVolume();
+
+    // 音の大きさに応じてARオブジェクトの拡大率を設定
+    double scale = 0.1 + volume * 0.9;
+
+    final node = _getNodeFromFlutterAsset(position, scale);
+    arkitController.add(node);
+  }
+
+  ARKitGltfNode _getNodeFromFlutterAsset(vector.Vector3 position, double scale) =>
+      ARKitGltfNode(
+        assetType: AssetType.flutterAsset,
+        url: 'https://hogehoge.com/3dmodel.glb',
+        scale: vector.Vector3(scale, scale, scale),
+        position: position,
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart'; // JSON文字列を扱うために必要
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sound_classify_app/firebase_options.dart';
 import 'package:sound_classify_app/main_app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MainApp()));
 }
+```
