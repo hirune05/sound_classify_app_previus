c9d68f9729672b99652d7ff1eeb740a8004e4375
TajimaMiho <kapibaramihho@gmail.com>
2024-03-23 01:10:25 +0900

    ar

--- frontend/lib/ui/pages/ar_page/ar_page.dart
+++ frontend/lib/ui/pages/ar_page/ar_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sound_classify_app/controllers/recording_page_controller.dart';
+import 'package:sound_classify_app/models/detail.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
 // 音声認識のデータから音の大きさを取得する関数
@@ -9,44 +12,51 @@ double getSoundVolume() {
   return Random().nextDouble();
 }
 
-class ArPage extends StatelessWidget {
+class ArPage extends ConsumerWidget {
+  const ArPage({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sounds = ref.watch(recordingProvider).detail?.sounds;
     return Scaffold(
       appBar: AppBar(title: const Text('AR')),
       body: ARKitSceneView(
         showFeaturePoints: true,
         enableTapRecognizer: true,
         planeDetection: ARPlaneDetection.horizontalAndVertical,
-        onARKitViewCreated: (controller) => _onARKitViewCreated(controller),
+        onARKitViewCreated: (controller) =>
+            _onARKitViewCreated(controller, sounds),
       ),
     );
   }
 
-  void _onARKitViewCreated(ARKitController arkitController) {
-    addSixNodes(arkitController);
+  void _onARKitViewCreated(
+      ARKitController arkitController, Map<String, double>? sounds) {
+    addSixNodes(arkitController, sounds);
   }
 
-  void addSixNodes(ARKitController arkitController) {
-    addNode(arkitController, 'assets/glb/Tree.glb');
-    addNode(arkitController, 'assets/glb/Tree.glb');
-    addNode(arkitController, 'assets/glb/Tree.glb');
-    addNode(arkitController, 'assets/glb/Tree.glb');
-    addNode(arkitController, 'assets/glb/Tree.glb');
+  void addSixNodes(
+      ARKitController arkitController, Map<String, double>? sounds) {
+    final dishVolume = sounds?['皿の音'];
+    final electronicVolume = sounds?['機械音'];
+    final fanVolume = sounds?['換気扇の音'];
+    final keybordVolume = sounds?['キーボードの音'];
+    final speakingVolume = sounds?['話し声'];
+    addNode(arkitController, 'assets/glb/cone.glb', dishVolume!);
+    addNode(arkitController, 'assets/glb/coral.glb', electronicVolume!);
+    addNode(arkitController, 'assets/glb/mofu.glb', fanVolume!);
+    addNode(arkitController, 'assets/glb/Rock.glb', keybordVolume!);
+    addNode(arkitController, 'assets/glb/Rock2.glb', speakingVolume!);
   }
 
-  void addNode(ARKitController arkitController, String url) {
+  void addNode(ARKitController arkitController, String url, double volume) {
     final position = vector.Vector3(
       Random().nextDouble() * 0.5,
       Random().nextDouble() * 0.5,
       Random().nextDouble() * 0.5,
     );
 
-    // 音声認識のデータから音の大きさを取得
-    double volume = getSoundVolume();
-
     // 音の大きさに応じてARオブジェクトの拡大率を設定
-    double scale = 0.1 + volume * 0.9;
+    double scale = 0.1 + volume * 0.05;
 
     final node = _getNodeFromFlutterAsset(position, scale, url);
     arkitController.add(node);
