44ce0de86610169c6423dd360e4194607da586ae
TajimaMiho <kapibaramihho@gmail.com>
2024-03-29 10:38:43 +0900

    didn't hearing

--- frontend/lib/ui/pages/hearing_page/hearing_page.dart
+++ frontend/lib/ui/pages/hearing_page/hearing_page.dart
@@ -1,44 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'package:path_provider/path_provider.dart';
 
 class HearingPage extends StatefulWidget {
   const HearingPage({Key? key}) : super(key: key);
-
   @override
   State<HearingPage> createState() => _HearingPageState();
 }
 
 class _HearingPageState extends State<HearingPage> {
-  final audioPlayer = AudioPlayer();
-
-  @override
-  void initState() {
-    super.initState();
-    _startPlaying();
-  }
-
-  @override
-  void dispose() {
-    audioPlayer.release();
-    super.dispose();
+  Future<void> playAudio() async {
+    final audioPlayer = AudioPlayer();
+    audioPlayer.play(AssetSource('audio/sample.m4a'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('聞いて体験'),
-      ),
-      body: const Center(
-        child: Text('音声解析ページ'),
-      ),
-    );
-  }
-
-  // 再生開始
-  Future<void> _startPlaying() async {
-    // 再生開始
-    await audioPlayer.play(AssetSource('aftersample.m4a'));
+        appBar: AppBar(
+          title: const Text('聞いて体験'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  '録音をはじめる',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      return Colors.green; // Use the component's default.
+                    },
+                  ),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(80, 80)),
+                  shadowColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      return states.contains(MaterialState.pressed)
+                          ? Colors.transparent
+                          : Colors.black; // Use the component's default.
+                    },
+                  ),
+                  elevation: MaterialStateProperty.resolveWith<double>(
+                    (Set<MaterialState> states) {
+                      return states.contains(MaterialState.pressed)
+                          ? 0
+                          : 10; // Shadow when not pressed.
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(height: 150),
+              ElevatedButton(
+                onPressed: playAudio,
+                child: Text(
+                  '再生する',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      return Colors.green; // Use the component's default.
+                    },
+                  ),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(80, 80)),
+                  shadowColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      return states.contains(MaterialState.pressed)
+                          ? Colors.transparent
+                          : Colors.black; // Use the component's default.
+                    },
+                  ),
+                  elevation: MaterialStateProperty.resolveWith<double>(
+                    (Set<MaterialState> states) {
+                      return states.contains(MaterialState.pressed)
+                          ? 0
+                          : 10; // Shadow when not pressed.
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
